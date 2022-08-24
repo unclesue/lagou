667ebabba0b71db9ae73bed621fe257bfe73d978
sue <sx_syj@163.com>
2022-08-25 06:16:56 +0800

    防止xss攻击

--- react/react-ssr/src/share/store/actions/user.js
+++ react/react-ssr/src/share/store/actions/user.js
@@ -3,12 +3,12 @@ import axios from "axios";
 export const SAVE_USER = "save_user";
 
 export const fetchUser = () => async (dispatch) => {
-  // const { data } = await axios.get(
-  //   "https://jsonplaceholder.typicode.com/users"
-  // );
-  const data = [{
-    id: 1,
-    name: "</script><script>alert(1)</script>"
-  }]
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  // const data = [{
+  //   id: 1,
+  //   name: "</script><script>alert(1)</script>"
+  // }]
   dispatch({ type: SAVE_USER, payload: data });
 };
