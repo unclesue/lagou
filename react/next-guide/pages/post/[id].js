ea227c60ebd21687ad261c48691495967ae47185
sue <sx_syj@163.com>
2022-08-27 05:50:07 +0800

    fallback/自定义404

--- react/next-guide/pages/post/[id].js
+++ react/next-guide/pages/post/[id].js
@@ -2,7 +2,11 @@
 // 基于参数为页面组件生成html页面，有多少参数就生成多少html页面
 // 在构建应用时，先获取用户可以访问的所有路由参数，再根据路由参数获取具体数据，然后根据数据生成静态html
 
+import { useRouter } from "next/router";
+
 export default function Post({ data }) {
+  const route = useRouter();
+  if (route.isFallback) return <div>loading</div>;
   return (
     <div>
       <h1>
@@ -14,6 +18,7 @@ export default function Post({ data }) {
 
 // 返回路由参数所对应的具体数据
 export async function getStaticProps({ params }) {
+  console.log("getStaticProps");
   let data;
   switch (params.id) {
     case "1":
@@ -21,6 +26,9 @@ export async function getStaticProps({ params }) {
       break;
     case "2":
       data = { id: params.id, title: "title 2" };
+      break;
+    case "3":
+      data = { id: params.id, title: "title 3" };
       break;
     default:
       data = {};
@@ -34,9 +42,11 @@ export async function getStaticProps({ params }) {
 }
 
 // 返回用户可以访问的所有路由参数
+// fallback=false 如果客户端发送了一个不在范围内的请求参数，则展示404页面
+// fallback=true 服务器会再次执行getStaticProps，根据参数获取数据，再进行静态生成
 export async function getStaticPaths() {
   return {
     paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
-    fallback: false,
+    fallback: true,
   };
 }
