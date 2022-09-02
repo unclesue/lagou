268d0a4686543e54596d88e9ec0d3c80e3706615
suxi <sx_syj@163.com>
2022-09-02 10:14:59 +0800

    strapi和gatsby增加配置文件

--- react/gatsby-guide/gatsby-config.js
+++ react/gatsby-guide/gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   /* Your site config here */
@@ -28,8 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-images`],
-        extensions: [`.md`]
+        plugins: [`gatsby-remark-images`]
       },
     },
     `gatsby-transformer-json`,
@@ -38,8 +40,8 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "http://localhost:1337",
-        accessToken: "5c7efa0e40a23fefccbf0547702c0a7b2b19dfd7ae8699a51d18b3592ba6d6f2be5f782440cf814bc1a7c1e5b4f19af2af1434b8979a429b9d81f6fcfc0baf05537e53841a73718a1b3dd36e32c271609e7b9ad5a898fe02375c6f1820b67e427a1dcde745b7fac64cbe3f8b29039f20f87d4674bb562cab1682cbe64250542b",
+        apiURL: process.env.STRAPI_API_URL,
+        accessToken: process.env.STRAPI_TOKEN,
         collectionTypes: ["post"],
         singleTypes: [],
       },
